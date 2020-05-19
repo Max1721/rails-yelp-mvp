ca46b3f0ad9485397610dfdf57aa3700b5aeca88
Maxime Berthelot <maxime@buffer.com>
2020-05-19 20:42:52 +0200

    Restaurant & Review model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "06 76 23 80 65", category: "chinese" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "06 76 23 80 65", category: "italian" }
+east_mamma = { name: "East Mamma", address: "124 avenue du faubourg St Antoine, 75011 Paris", phone_number: "06 76 23 80 65", category: "italian" }
+la_popotte = { name: "La Popotte", address: "2 rue de Levis, 75017, Paris", phone_number: "06 68 98 09 67", category: "french" }
+nem_dore = { name: "Nem Dore", address: "2 avenue Foch, 75008, Paris", phone_number: "06 68 98 09 67", category: "chinese" }
+
+[dishoom, pizza_east, east_mamma, la_popotte, nem_dore].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
